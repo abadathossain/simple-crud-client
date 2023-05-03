30aa339b9926071b8bdb6b717ca9b7bb17c047b5
Programming Hero <programming.hero1@gmail.com>
2023-05-03 10:28:56 -0500

    added update user route

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       .then(data => {
         console.log(data);
         if (data.insertedId) {
-          alert('Users added successfully')
+          alert('Users added successfully');
           form.reset();
         }
       })
